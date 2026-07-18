2ab9013496c3257d26006522fe6e34278f5cd016
本多晃一朗 <kou0903@Mac.nkano1.kt.home.ne.jp>
2026-07-18 15:39:32 +0900

    feat: jsで完了、削除ボタンを追加

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,6 @@
 const onClickAdd = () =>{
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+  // alert(inputText);
   document.getElementById("add-text").value = "";
 
   const li = document.createElement("li");
@@ -13,7 +13,23 @@ const onClickAdd = () =>{
   p.className = "todo-item";
   p.innerText = inputText;
 
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.classList.add('rounded', 'border-none', 'px-[2vw]', 'mx-[1vw]', 'md-[1vh]', 'bg-gray-100', 'hover:bg-[#79a8a9]', 'hover:text-[#fff]', 'hover:cursor-pointer');
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  })
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.classList.add('rounded', 'border-none', 'px-[2vw]', 'bg-gray-100', 'hover:bg-[#79a8a9]', 'hover:text-[#fff]', 'hover:cursor-pointer');
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  })
+
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
   // console.log(li);
 
